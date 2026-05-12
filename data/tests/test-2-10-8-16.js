bbe69cb8064c0ae50d32d67d2605608cb6d2a571
Nikodane <zholamannurbol01@gmail.com>
2026-05-13 01:57:09 +0500

    Add files via upload

---
+++ data/tests/test-2-10-8-16.js
@@ -0,0 +1,148 @@
+window.TEST_BANK = window.TEST_BANK || {};
+
+window.TEST_BANK.test_2_10_8_16 = {
+  // Шаблон:
+  // text: "Текст вопроса",
+  // options: ["Вариант 1", "Вариант 2", "Вариант 3", "Вариант 4"],
+  // answer: 0 // индекс правильного ответа (с нуля)
+  questions: [
+  {
+    text: "Year of invention of the telephone?",
+    options: ["1860", "1837", "1876", "1947", "2001"],
+    answer: 2, // 1876
+  },
+
+  {
+  text: "First e-mail program created in….",
+  options: ["1987", "2020", "1945", "2011", "1971"],
+  answer: 4, // 1971
+},
+{
+  text: "what is the value of the number 17 in the Octal system in the binary system?",
+  options: ["10101", "111", "100001", "10011", "11001"],
+  answer: 4, // 11001
+},
+{
+  text: "convert 15 in decimal to binary:",
+  options: ["1000", "1101", "1010", "1111", "1001"],
+  answer: 3, // 1111
+},
+{
+  text: "Conversion of a hexadecimal number 3D to binary number is",
+  options: [
+    "Noneof these",
+    "111101",
+    "11000110",
+    "110011",
+    "110001",
+  ],
+  answer: 1, // 111101
+},
+{
+  text: "Year of invention of the telegraph?",
+  options: ["1876", "1947", "2001", "1860", "1837"],
+  answer: 4, // 1837
+},
+{
+  text: "what number is obtained when converting 11001 in binary to decimal?",
+  options: ["18", "19", "21", "15", "17"],
+  answer: 4, // 17
+},
+{
+  text: "what is the value of 1011 in the binary number system in the decimal system?",
+  options: ["13", "9", "6", "5", "11"],
+  answer: 0, // 13
+},
+{
+  text: "The binary equivalent of the Hexadecimal number 7BD is",
+  options: [
+    "11110111101",
+    "111010111101",
+    "100001000100",
+    "100010111101",
+    "111101111010",
+  ],
+  answer: 0, // 11110111101
+},
+{
+  text: "Conversion of an octal number 73 to binary number is",
+  options: [
+    "111100",
+    "10111",
+    "111011",
+    "110010",
+    "Noneof these",
+  ],
+  answer: 2, // 111011
+},
+{
+  text: "The binary equivalent of the Hexadecimal number 74A is",
+  options: [
+    "11111111010",
+    "11101001010",
+    "11110111111",
+    "100010111101",
+    "111111101",
+  ],
+  answer: 1, // 11101001010
+},
+{
+  text: "Conversion of a hexadecimal number D8 to binary number is",
+  options: [
+    "10101101",
+    "10001101",
+    "11011000",
+    "11001100",
+    "Noneof these",
+  ],
+  answer: 2, // 11011000
+},
+{
+  text: "When did World Wide Web introduce?",
+  options: ["1837", "2014", "1850", "1990", "1860"],
+  answer: 3, // 1990
+},
+{
+  text: "Conversion of an octal number 106 to binary number is",
+  options: [
+    "1110010",
+    "10000110",
+    "Noneof these",
+    "1000110",
+    "1100110",
+  ],
+  answer: 3, // 1000110
+},
+{
+  text: "what number is obtained when converting the number 23 in the Octal system to the binary system?",
+  options: ["11101", "100001", "10011", "11001", "10001"],
+  answer: 3, // 11001
+},
+{
+  text: "what is the value of the number B in the hexadecimal system in the binary system?",
+  options: ["1001", "1011", "1010", "1101", "1110"],
+  answer: 1, // 1011
+},
+{
+  text: "The decimal equivalent of the binary number 100101 is",
+  options: ["73", "33", "46", "28", "37"],
+  answer: 4, // 37
+},
+{
+  text: "what number comes up when you convert 100110 in binary to octal?",
+  options: ["64", "31", "56", "55", "45"],
+  answer: 2, // 56
+},
+{
+  text: "The binary equivalent of the Octal number 13 is",
+  options: [
+    "1011",
+    "1101",
+    "1111",
+    "1010",
+    "1100"
+  ],
+  answer: 0
+},
+  ],
+};
